77bfd8acaf3ce82309f0eeea6a06ec8217be7bce
jeffrycascate <jeffry.cascante@ucrso.info>
2024-08-11 00:51:13 -0600

    Instalcion de beyoncomper

--- main.sh
+++ main.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
 download_dir="$HOME/Downloads"
 
+base_url="https://www.scootersoftware.com"
 # URL de la página donde se encuentra el botón
-url="https://netbeans.apache.org/front/main/download/"
+url="https://www.scootersoftware.com/download"
 
 # Descargar el contenido de la página
 page=$(wget -qO- "$url")
 
-# Extraer la línea que contiene el enlace con la clase "button success"
-button_href=$(echo "$page" | grep -oP '<a[^>]*class="[^"]*button success[^"]*"[^>]*href="[^"]*"' | grep -oP 'href="[^"]*"' | sed 's/href="\([^"]*\)"/\1/')
+# Extraer el valor del href que contiene el texto "Debian"
+relative_href=$(echo $page | grep -oP '(?<=<a href=")[^"]*(?="[^>]*>Debian</a>)')
+echo "valor $relative_href"
+# Combinar la URL base con el href relativo
 
-# Comprobar si se ha encontrado el atributo href
-if [ -z "$button_href" ]; then
-    echo "No se encontró un atributo href en el enlace con la clase 'button success'."
-    exit 1
-fi
+full_url="${base_url}${relative_href}"
+echo "valor $full_url"
+wget "$full_url" -O "$download_dir/comper.deb"
 
-# Reemplazar "../../../" con "https://netbeans.apache.org/"
-final_url=$(echo "$button_href" | sed 's#^\.\./\.\./\.\./#https://netbeans.apache.org/#')
-
-url=$final_url
-
-# Descargar el contenido de la página
-page=$(wget -qO- "$url")
-
-# Buscar la etiqueta <a> que tiene un href que termina en .deb
-deb_link=$(echo "$page" | grep -oP '<a[^>]*href="[^"]*\.deb"[^>]*>.*?</a>')
-
-# Comprobar si se ha encontrado algún enlace
-if [ -z "$deb_link" ]; then
-    echo "No se encontró ninguna etiqueta <a> con un href que termine en .deb."
-else 
-    href=$(echo $deb_link | grep -oP '(?<=href=")[^"]*')
-    page=$(wget -qO- "$href")
-    url=$(echo $page | grep -oP '(?<=<strong>)[^<]*\.deb' | head -n 1) 
-    wget "$url" -O "$download_dir/netbeans.deb"
-
-    # Instalar el archivo .deb
-    sudo dpkg -i "$download_dir/netbeans.deb" 
-fi
+# Instalar el archivo .deb
+sudo dpkg -i "$download_dir/comper.deb" 
+sudo apt-get install -f -y
--- post_installation.sh
+++ post_installation.sh
@@ -133,6 +133,27 @@ else
     sudo apt-get install -f 
 fi
 
+#Instacion de beyonecomper
+base_url="https://www.scootersoftware.com"
+# URL de la página donde se encuentra el botón
+url="https://www.scootersoftware.com/download"
+
+# Descargar el contenido de la página
+page=$(wget -qO- "$url")
+
+# Extraer el valor del href que contiene el texto "Debian"
+relative_href=$(echo $page | grep -oP '(?<=<a href=")[^"]*(?="[^>]*>Debian</a>)')
+echo "valor $relative_href"
+# Combinar la URL base con el href relativo
+
+full_url="${base_url}${relative_href}"
+echo "valor $full_url"
+wget "$full_url" -O "$download_dir/comper.deb"
+
+# Instalar el archivo .deb
+sudo dpkg -i "$download_dir/comper.deb" 
+sudo apt-get install -f -y
+
 
 # code --list-extensions | xargs -I {} echo '"{}"'
 extensions=(
